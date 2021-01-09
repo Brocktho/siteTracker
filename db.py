92aa97b2aa281f1cf76baf85a9dd09d6e948ecb0
Brocktho <71537341+Brocktho@users.noreply.github.com>
2021-01-09 18:02:36 -0500

    let connect=True

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 
 
-client = pymongo.MongoClient("mongodb+srv://Brock:Rocketspaceship9126@sitetracker.g9j37.mongodb.net/SiteTracker?retryWrites=true&w=majority", port=27017)
+client = pymongo.MongoClient("mongodb+srv://Brock:Rocketspaceship9126@sitetracker.g9j37.mongodb.net/SiteTracker?retryWrites=true&w=majority", port=27017, connect=True)
 db = client.get_database("SiteTracker")
 user_collection = pymongo.collection.Collection(db, 'Websites')
 
